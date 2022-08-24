d6797b4eb7a185cc8a15199c32c61547b5298616
Anthony <asingiser@gmail.com>
2022-08-24 11:49:22 -0400

    add components, create grid structure for notes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
+import NotesList from './components/Noteslist'
+import {useState} from 'react';
+import {nanoid} from 'nanoid';
+
 function App() {
+  const [notes, setNotes] = useState([
+    {
+      id: nanoid(),
+      text: 'text of the first note',
+      date: '08/23/2022',
+    },
+    {
+      id: nanoid(),
+      text: 'text of the second note',
+      date: '08/23/2022',
+    },
+    {
+      id: nanoid(),
+      text: 'text of the third note',
+      date: '08/23/2022',
+    },
+    {
+      id: nanoid(),
+      text: 'text of the fourth note',
+      date: '08/23/2022',
+    },
+    {
+      id: nanoid(),
+      text: 'text of the fifth note',
+      date: '08/23/2022',
+    },
+]);
   return( 
-    <div className='App'>
-      Hello React!
+    <div className='container'>
+      <NotesList notes = {notes} />
     </div>
   )
 }
