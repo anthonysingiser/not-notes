dd08449685140da98f879f57bf62738d3eb606e4
chrysanthemum <asingiser@gmail.com>
2022-08-29 13:44:49 -0400

    update remote to local

--- src/components/AddNote.js
+++ src/components/AddNote.js
@@ -1,10 +1,18 @@
+import { useState } from "react"
+
 const AddNote = () => {
+    const [noteText, setNoteText] = useState('')
+
+    const handleChange = (e) =>{
+        console.log(e.target.value)
+    }
     return (
         <div className='note new'>
             <textarea
                 rows='8'
                 cols='10'
                 placeholder='Type to add a note'
+                onChange={handleChange}
             ></textarea>
             <div className="note-footer">
                 <small>200 Remaining</small>
